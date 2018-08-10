cfb98aed46d041a73701cc038f07dc38f511f6ff
Heider Lopes <heidertreinamentos@gmail.com>
2018-08-10 09:40:19 -0300

    Criacao da tela de detalhe.

---
+++ app/src/main/java/br/com/fiap/pokedex/DetalheActivity.kt
@@ -0,0 +1,12 @@
+package br.com.fiap.pokedex
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DetalheActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalhe)
+    }
+}
